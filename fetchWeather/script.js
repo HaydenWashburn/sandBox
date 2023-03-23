b764ca189e22cb3083ca94e7aa2cdc085652c707
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-23 15:05:16 -0500

    created openWeather folder with new API

---
+++ fetchWeather/script.js
@@ -0,0 +1,11 @@
+const apiKey = "011ceaa673e608d33292e3f8d37fa170";
+const cityname = "New Orleans";
+const state = "LA";
+const countryCode = "US"
+const limit = "5"
+
+const geoURL = `api.openweathermap.org/geo/1.0/direct?q=${cityname},${state},${countryCode}&limit=${limit}&appid=${apiKey}`
+function getCityWeather(){
+    fetch()
+
+}
